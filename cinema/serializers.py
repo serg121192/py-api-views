26d66e8a1be092f612c8fd36499f9e5fb097c72d
srx <srx92@protonmail.com>
2025-02-13 16:56:10 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -89,7 +89,13 @@ class ActorSerializer(serializers.Serializer):
         return Actor.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.first_name = validated_data.get("first_name", instance.first_name)
-        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.first_name = validated_data.get(
+            "first_name",
+            instance.first_name
+        )
+        instance.last_name = validated_data.get(
+            "last_name",
+            instance.last_name
+        )
         instance.save()
         return instance
